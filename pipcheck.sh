9d26cbfca9b5aabbb4ef1e072210dfdd6658e0ee
Rojan Shakya <shakya.rojan@gmail.com>
2015-12-22 18:13:47 -0500

    Added a variable to store the public IP

--- pipcheck.sh
+++ pipcheck.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #There is a crontab entry to run this script every hour
+
+#Finding the public IP
+MYIP=$(wget http://ipinfo.io/ip -qO -)
 if [ "$MYIP" = "`cat ~/lkpip.txt`" ]; then
     exit 0
 else
     echo "Your new Public IP is: $MYIP"|mail -s "Public IP has changed" myrpialerts@gmail.com
-    echo $MYIP > lkpip.txt
+    echo $MYIP > /home/pi/lkpip.txt
 fi
